3b5bf52abc080c0bcbab7a0a1089baea534be65e
Edwardose <lavoroedoardo@gmail.com>
2020-04-30 02:24:11 +0200

    test

--- YouClothes/PHP files/login.php
+++ YouClothes/PHP files/login.php
@@ -2,38 +2,117 @@
      $dbconn = pg_connect("host=localhost port=5432 dbname=YouClothes user=postgres password=edoardo97")
      or die('Could not connect: '.pg_last_error());
      $email = $_POST['email'];   //prendo il parametro 'email' della form inviata
-     $q1 = "select from utente where email=$1";  //il risultato della query viene inserito dentro $1
+     $q1 = "select * from utente where email=$1";  //il risultato della query viene inserito dentro $1
      $result = pg_query_params($dbconn,$q1,array($email));   //funzione per far effettuare le query e inserire i risultati dentro array (in questo caso solo 1: email)
-     //controllo se utente con 'email' esiste
+     //controllo se utente con 'email'  non esiste
      if(!($line=pg_fetch_array($result,null,PGSQL_ASSOC))){ //ovvero controllo che l'oggetto non esiste (not pg_fet...)
-        //questa parte sottostante sarà da cambiare. Per adesso ci limitiamo a questo
-        //perchè in teoria deve riportarti alla pagina iniziale solo che al posto di 'Sign in/Register' ci deve essere il tuo nickname
-        echo "<h1>Sembra che il tuo account non esista! 
-        Creane uno 
-        <a href=../Registrazione/signup.html>CLICCANDO QUI </a>";
+        echo "<html>
+
+                <head>
+                    <link rel=stylesheet href=../stile.css type=text/css>
+                    <title>ERRORE</title>
+                    <meta charset=utf-8>
+                    <meta name=viewport content=width=device-width, initial-scale=1>
+                </head>
+            
+            
+                <body class=bordo>
+            
+                    <p align=center> <br><br><br><br>
+                        <titolo>Sembra che il tuo account non esista oppure l'email immessa è errata!</titolo> <br><br>
+                        <sottotitolo>
+                            Crea un account premendo
+                            <a href=../Registrazione/signup.html>QUI </a>
+                        </sottotitolo> <br><br>
+                        <sottotitolo> 
+                            Oppure ritenta il
+                            <a href=../Registrazione/login.html>LOGIN </a>
+                        </sottotitolo> <br><br>
+                    </p>
+            
+                </body>
+            
+            </html>";
     }
     //se esiste utente con 'email', allora controllo se la password è corretta
     else{
         $email = $_POST['email'];
         $pass = $_POST['password'];
-        $q1 = "select from utente where email=$1 and password=$2";
-        $result = pg_query_params($dbconn,$q1,array($email,$password));
+        $q1 = "select * from utente where email=$1 and password=$2";
+        $result = pg_query_params($dbconn,$q1,array($email,$pass));
         if($line=pg_fetch_array($result,null,PGSQL_ASSOC)){
-            echo "<h1>
-            Bene! Hai appena effettuato il Login! 
+            /*echo "<h1>
+            Bene! Hai appena effettuato il Login! <br><br>
             Premi 
             <a href=../Home/homepage.html>QUI </a>
             per iniziare a usare il sito YouClothes!
-            </h1>";
+            </h1>";*/
+
+            //
+
+            echo "<html>
+
+            <head>
+                <link rel=stylesheet href=../stile.css type=text/css>
+                <title>LOGIN EFFETTUATO</title>
+                <meta charset=utf-8>
+                <meta name=viewport content=width=device-width, initial-scale=1>
+            </head>
+        
+        
+            <body class=bordo>
+        
+                <p align=center> <br><br><br><br>
+                    <titolo>Hai appena effettuato il LOGIN!</titolo> <br><br>
+                    <sottotitolo>
+                        Premi
+                        <a href=../Home/homepage.html>QUI </a>
+                        per iniziare ad usare il sito YouClothes!
+                    </sottotitolo> <br><br>
+                </p>
+        
+            </body>
+        
+        </html>";
+        //
         }
+        //se la password non è corretta
         else{
-            echo "<h1>
-            L'Email o la Password non sono corretti! 
+            /*echo "<h1>
+            La Password non è corretta! 
             <br><br>
             Scegliere una delle opzioni sottostanti: <br>
-            <a href=..>RIPROVA LOGIN </a>&nbsp;&nbsp; 
-            <a href=../Home/homepage.html>RITORNA AL SITO </a>&nbsp;&nbsp; 
-            <a href=../signup.html>REGISTRATI</a>";
+            <a href=../Registrazione/login.html>RIPROVA LOGIN </a>&nbsp;&nbsp; 
+            <a href=../Home/homepage.html>RITORNA AL SITO </a>&nbsp;&nbsp;";
+            */
+            //
+            echo "<html>
+
+            <head>
+                <link rel=stylesheet href=../stile.css type=text/css>
+                <title>ERRORE</title>
+                <meta charset=utf-8>
+                <meta name=viewport content=width=device-width, initial-scale=1>
+            </head>
+        
+        
+            <body class=bordo>
+
+                <p align=center><br><br><br>
+                    <titolo>
+                        La Password non è corretta!
+                    </titolo><br><br>
+                    <sottotitolo>
+                        Scegli una delle opzioni sottostanti: 
+                    </sottotitolo><br><br>
+                    <sottotitolo>
+                        <a href=../Registrazione/login.html>RIPROVA LOGIN </a>&nbsp;&nbsp; 
+                        <a href=../Home/homepage.html>RITORNA AL SITO </a>&nbsp;&nbsp;
+                    <sottotitolo>
+
+            </body>
+            </html>";
+            //
         }
     }
 ?>
--- YouClothes/PHP files/registrazione.php
+++ YouClothes/PHP files/registrazione.php
@@ -3,68 +3,72 @@
         <?php
             $dbconn = pg_connect("host=localhost port=5432 dbname=YouClothes user=postgres password=edoardo97")
             or die('Could not connect: '.pg_last_error());
-            //controllo se stai già provando a registrarti da altre pagine
-            //serve per controllare che la form 'registrazione' è stata chiamata solo durante la pagina di registrazione
-            /*if(!(isset($_POST['registrazione']))){
-                //header('Location: ../Home/homepage.html');
-                echo "<h1>DIOCANE</h1>";
-            }
-            else{*/
-                //controllo se utente esiste già
-                $email = $_POST['email'];   //prendo il parametro 'email' della form inviata
-                $q1 = "select from utente where email=$1";  //il risultato della query viene inserito dentro $1
-                $result = pg_query_params($dbconn,$q1,array($email));   //funzione per far effettuare le query e inserire i risultati dentro array (in questo caso solo 1: email)
-                if($line=pg_fetch_array($result,null,PGSQL_ASSOC)){ //controlla se l'utente con valore '$email' è già presente nel database 
-                    //header("Location: URL da fare in futuro per reindirizzare in una pagina più consona");
-                    //per adesso usiamo ciò che sta qui sotto
-                    echo "<h1>
-                    Operazione Annullata! Questo Utente esiste già!
-                    Clicca 
-                    <a href=../Registrazione/login.html>QUI </a>
-                    per effettuare il loging </h1>";
-                }
-                else{
-                    //Operazioni per inserire i nuovi valori nel DB, tabella Utente -> registrazione nuovo Utente
-                    $email=$_POST['email'];
-                    $nome=$_POST['nome'];
-                    $cognome=$_POST['cognome'];
-                    $nickname=$_POST['nickname'];
-                    $password=$_POST['password'];  //md5 per 'ashare' le password in modo tale da non poterle vedere nel database
-                    $q2="insert into utente values ($1,$2,$3,$4,$5)";   //query per inserire valori all'interno della tabella Utente
-                    $data=pg_query_params($dbconn,$q2,array($email,$nome,$cognome,$nickname,$password));   //operazione per effettuare la query e inserire i dati all'interno di 'array'
-                    if($data){
-                        //header("Location: URL da fare in futuro per reindirizzare in una pagina più consona");
-                        //per adesso usiamo ciò che sta qui sotto
-                        echo "<html>
+            //controllo se utente esiste già
+            $email = $_POST['email'];   //prendo il parametro 'email' della form inviata
+            $q1 = "select * from utente where email=$1";  //il risultato della query viene inserito dentro $1
+            $result = pg_query_params($dbconn,$q1,array($email));   //funzione per far effettuare le query e inserire i risultati dentro array (in questo caso solo 1: email)
+            $line=pg_fetch_array($result,null,PGSQL_ASSOC);
+            if($line){ //controlla se l'utente con valore '$email' è già presente nel database 
+                echo "<html>
 
-                        <head>
-                            <link rel=stylesheet href=../stile.css type=text/css>
-                            <link rel=stylesheet href=../css/bootstrap.min.css>
-                            <link href=../css/bootstrap-responsive.css rel=stylesheet>
-                            <script src=https://code.jquery.com/jquery-2.1.3.min.js></script>
-                            <script src=../js/bootstrap.min.js></script>
-                    
-                            <title>Sign up completed</title>
-                            <meta charset=utf-8>
-                            <meta name=viewport content=width=device-width, initial-scale=1>
-                        </head>
-                    
-                    
-                        <body class=bordo>
-                    
-                            <p align=center> <br><br><br><br>
-                                <titolo>Registrazione completata!</titolo> <br><br>
-                                <sottotitolo>Complimenti, registrazione effettuata con successo! </sottotitolo> <br>
-                                <sottotitolo> Clicca qui per tornare al menu principale: </sottotitolo> <br><br>
-                                <a href=../Home/homepage.html>Home</a>
-                            </p>
-                    
-                        </body>
-                    
-                    </html>";
-                    }
+                    <head>
+                        <link rel=stylesheet href=../stile.css type=text/css>
+                        <title>ERRORE</title>
+                        <meta charset=utf-8>
+                        <meta name=viewport content=width=device-width, initial-scale=1>
+                    </head>
+                
+                
+                    <body class=bordo>
+                
+                        <p align=center> <br><br><br><br>
+                            <titolo>Operazione Annullata! Questo Utente esiste già!</titolo> <br><br>
+                            <sottotitolo>Clicca 
+                            <a href=../Registrazione/login.html>QUI</a> 
+                            per effettuare il login </sottotitolo> <br>
+                            <sottotitolo>Clicca 
+                            <a href=../Registrazione/signup.html>QUI</a> 
+                            per ri-effettuare la registrazione </sottotitolo> <br>
+                        </p>
+                
+                    </body>
+                
+                </html>";
+            }
+            else{
+                //Operazioni per inserire i nuovi valori nel DB, tabella Utente -> registrazione nuovo Utente
+                $email=$_POST['email'];
+                $nome=$_POST['nome'];
+                $cognome=$_POST['cognome'];
+                $nickname=$_POST['nickname'];
+                $password=$_POST['password'];  //md5 per 'ashare' le password in modo tale da non poterle vedere nel database
+                $q2="insert into utente values ($1,$2,$3,$4,$5)";   //query per inserire valori all'interno della tabella Utente
+                $data=pg_query_params($dbconn,$q2,array($email,$nome,$cognome,$nickname,$password));   //operazione per effettuare la query e inserire i dati all'interno di 'array'
+                if($data){
+                    echo "<html>
+
+                    <head>
+                        <link rel=stylesheet href=../stile.css type=text/css>
+                        <title>REGISTRAZIONE</title>
+                        <meta charset=utf-8>
+                        <meta name=viewport content=width=device-width, initial-scale=1>
+                    </head>
+                
+                
+                    <body class=bordo>
+                
+                        <p align=center> <br><br><br><br>
+                            <titolo>Registrazione completata!</titolo> <br><br>
+                            <sottotitolo>Complimenti, registrazione effettuata con successo! </sottotitolo> <br>
+                            <sottotitolo> Clicca qui sotto per tornare al menu principale: </sottotitolo> <br><br>
+                            <a href=../Home/homepage.html>Home</a>
+                        </p>
+                
+                    </body>
+                
+                </html>";
                 }
-            //}
+            }
         ?>
     </body>
 </html>
