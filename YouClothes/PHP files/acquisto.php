60e69f2b6aa108605e594c84bd37e5c7ef924e74
andrea-prodon <salsedine98@gmail.com>
2020-05-18 11:36:50 +0200

    bug fixed

--- YouClothes/PHP files/acquisto.php
+++ YouClothes/PHP files/acquisto.php
@@ -9,58 +9,59 @@
             $dbconn = pg_connect("host=localhost port=5433 dbname=YouClothes user=postgres password=edoardo97")
             or die('Could not connect: '.pg_last_error());
 
-            $query = "update annuncio set disponibile=false where id=$id_annuncio";
-            $res = pg_query($dbconn, $query) or die('Query failed '.pg_last_error()); 
             
             $query_vend = "select utente_email from annuncio where id=$id_annuncio";
             $res_vend= pg_query($dbconn, $query_vend) or die('Query failed '.pg_last_error());
             $row_vend =pg_fetch_row($res_vend);
             $email_venditore = $row_vend[0];     //email del venditore dell annuncio
-
-            $query = "update annuncio set utente_email='$email' where id=$id_annuncio";
-            $res = pg_query($dbconn, $query) or die('Query failed '.pg_last_error()); 
-
+            
+            
             $query = "select prezzo from annuncio where id=$id_annuncio";
             $res = pg_query($dbconn, $query) or die('Query failed '.pg_last_error());
             $row = pg_fetch_row($res);
-            $prezzo=$row[0];
-
-
-
-            $utente = $_SESSION["nickname"];
+            $prezzo=$row[0];        //prezzo dell'articolo
+            
+            
+            $utente = $_SESSION["nickname"];    //nickname dell'utente
+            
             $q1="select saldo from utente where nickname ='$utente'";
-
             $res2=pg_query($dbconn,$q1) or die('Query failed '.pg_last_error());
             $row2 = pg_fetch_row($res2);
             $saldoutente=$row2[0];      //saldo del compratore
-
+            
             $aggiorna_saldo_compratore="update utente set saldo = $1 WHERE nickname = '$utente'";
             $prendi_saldo_venditore="select saldo from utente where email = '$email_venditore'";
             $aggiorna_saldo_venditore="update utente set saldo = $1 where email = '$email_venditore'";
-
+            
             $res_saldo_vend = pg_query($dbconn, $prendi_saldo_venditore);
             $row_saldo_vend = pg_fetch_row($res_saldo_vend);
             $saldo_venditore = $row_saldo_vend[0];      //saldo del venditore dell annuncio
-
+            
             if ($saldoutente >= $prezzo)  {             // se si hanno soldi a sufficienza
                 $nuovosaldo_compratore = $saldoutente - $prezzo;
                 $nuovosaldo_venditore = $saldo_venditore + $prezzo;
+                
+                $query = "update annuncio set utente_email='$email' where id=$id_annuncio";     //il proprietario diventa l'utente attuale
+                $res = pg_query($dbconn, $query) or die('Query failed '.pg_last_error()); 
+
+                $query = "update annuncio set disponibile=false where id=$id_annuncio";         //l'annuncio diventa non piu disponibile
+                $res = pg_query($dbconn, $query) or die('Query failed '.pg_last_error()); 
 
                 $data=pg_query_params($dbconn,$aggiorna_saldo_compratore,array($nuovosaldo_compratore)); 
                 $data2 = pg_query_params($dbconn, $aggiorna_saldo_venditore, array($nuovosaldo_venditore));
-
+                
                 if($data && $data2){
                     $_SESSION["saldo"]=$nuovosaldo_compratore;
                     echo "<html>
-
+                    
                     <head>
-                        <link rel=stylesheet href=../stile.css type=text/css>
-                        <title>Acquisto effettuato</title>
-                        <meta charset=utf-8>
-                        <meta name=viewport content=width=device-width, initial-scale=1>
+                    <link rel=stylesheet href=../stile.css type=text/css>
+                    <title>Acquisto effettuato</title>
+                    <meta charset=utf-8>
+                    <meta name=viewport content=width=device-width, initial-scale=1>
                     </head>
-                
-                
+                    
+                    
                     <body class=bordo>
                 
                         <p align=center> <br><br><br><br>
